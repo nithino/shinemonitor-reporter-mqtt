6dbeb84ebae7b4844a5a2451ec295e7bd721a7cd
nithino <nithino@gmail.com>
2025-06-30 04:23:12 +0530

    Update config.py
    http no longer works as the base url. It must be https. Otherwise, the script returns error. 
    Some websites do not provide company_key explicitly, but it can be identified in the souce of the webpage, after logging in.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 debug = False  # True to enable, False to disable
 
 # Shinemonitor settings
-base_url = 'http://android.shinemonitor.com/public/'
+base_url = 'https://android.shinemonitor.com/public/'
 usr = ''  # Username
 pwd = ''  # Password
-company_key = ''  # Company key. Obtained from portal
+company_key = ''  # Company key. Obtained from portal. It is bnrl_frRFjEz8Mkn for most makers. On the webpage, check view source, and search for company_key. You will find the right company key. 
 plant_id = ''  # Plant id (Power station ID). Obtained from portal.
 pn = ''  # Datalogger PN number. Obtained from portal
 sn = ''  # Device serial number. Obtained from portal
